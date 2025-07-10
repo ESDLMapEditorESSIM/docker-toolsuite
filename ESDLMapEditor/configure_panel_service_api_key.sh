da6fb662048e466c1a57f761ddd9500be08be54f
Ewoud Werkman <ewoud.werkman@tno.nl>
2025-07-10 14:32:44 +0200

    Add script to create API key for panel-service, to replace manual steps

---
+++ ESDLMapEditor/configure_panel_service_api_key.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+. ./panel_service.env
+if [ -z $GRAFANA_API_KEY ] || [ "$GRAFANA_API_KEY"="null" ] ; then
+  GRAFANA_API_KEY=$(curl --insecure -s -X POST -H "Content-Type: application/json" -d '{
+  "name": "panel-service",
+  "role": "Admin"
+}' -u admin:admin http://localhost:3000/api/auth/keys | jq -r '.key')
+   if [ "$GRAFANA_API_KEY"="null" ] ; then
+        echo "Failed to get API KEY"
+        exit 1
+   fi
+
+   echo "GRAFANA_API_KEY configured in panel_service.sh to: $GRAFANA_API_KEY"
+   echo "GRAFANA_API_KEY=$GRAFANA_API_KEY" > panel_service.env
+else
+   echo "GRAFANA_API_KEY already configured in panel_service.env"
+fi
